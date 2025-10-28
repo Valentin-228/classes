6ca71d6174cfd87fb58969482bab06b4114fa044
valen <orb49@mail.ru>
2025-10-28 09:25:35 +0300

    second

--- playwright_disabled.py
+++ playwright_disabled.py
@@ -1,1 +1,19 @@
-print('hello')
+class Group:
+    pupils = True
+    scholl_name = 42
+    director = "Mariyvanna"
+
+
+class PrimaryGroup(Group):
+    max_age = 11
+    min_age = 6
+    building_section = 'left'
+
+class HightGroup(Group):
+    max_age = 18
+    min_age = 14
+
+first_a = PrimaryGroup()
+eleven_a = HightGroup()
+
+
